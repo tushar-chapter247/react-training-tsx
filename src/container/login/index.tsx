ac5edc49192e939a95dd75244b1ba25eb5db9448
Bunny Molester <tushar462024@gmail.com>
2019-01-24 00:13:21 +0530

    create login page, working on css issues

---
+++ src/container/login/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import LoginPage from './login.page';
+
+class Login extends Component {
+  public render() {
+    return (
+      <>
+        <LoginPage />
+      </>
+    );
+  }
+}
+
+export default Login;
