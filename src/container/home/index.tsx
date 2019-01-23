932edafb4f33e342cf52b419a9f83daa8eb7c592
Bunny Molester <tushar462024@gmail.com>
2019-01-23 22:48:25 +0530

    Define types for State and Props

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import HomePage from './home.page';
 
-class Home extends Component {
+interface IState {
+  counter: number;
+}
+
+// tslint:disable-next-line:no-empty-interface
+interface IProps {}
+
+class Home extends Component<IProps, IState> {
+  public state = {
+    counter: 1,
+  };
+
   public render() {
     return (
       <>
