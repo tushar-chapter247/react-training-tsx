26fbd1c4fcbea3d02b5aa36d80930b339308365e
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-28 12:27:09 +0530

    Dispatch the value to actions so as to be updated in store by reducers

--- src/container/login/index.tsx
+++ src/container/login/index.tsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { userLogin } from '../../store/actions';
 import Nav from '../nav';
 import LoginPage from './login.page';
 
-class Login extends Component {
-  public state = {
+interface ILogin {
+  email: string;
+  password: string;
+}
+
+interface IDispatchFromProps {
+  loginActionDispatch: (payload: ILogin) => void
+}
+
+interface ILoginProps extends IDispatchFromProps {}; // EXTENDS APP PROPS WITH REDUX ACTIONS
+
+class Login extends Component<ILoginProps> {
+  public state: ILogin = {
     email: '',
     password: ''
   };
@@ -20,6 +34,7 @@ class Login extends Component {
       if (node.reportValidity()) {
         event.preventDefault();
         console.log('Login Form is valid! ', this.state.email, this.state.password);
+        this.props.loginActionDispatch({email: this.state.email, password: this.state.password}); // DISPATCH ACTION METHOD WITH PAYLOAD SO TO UPDATE IN STORE.
       }
     }
   };
@@ -40,89 +55,12 @@ class Login extends Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch: Dispatch): IDispatchFromProps => ({
+  loginActionDispatch: (payload: ILogin) => dispatch(userLogin(payload)), // THIS FUNCTION WILL BE AVAILABLE IN PROPS
+});
 
 
-
-
-
-// https://spin.atomicobject.com/2017/04/20/typesafe-container-components/
-// https://www.valentinog.com/blog/redux/
-
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { Dispatch } from 'redux';
-// import uuid from 'uuid';
-// import { addArticle } from '../../store/actions';
-// import Nav from '../nav';
-// import LoginPage from './login.page';
-
-// interface IArticle {
-//   name: string;
-//   id: string;
-// }
-
-// interface IClassProps {
-//   addArticles: (article: IArticle) => void;
-// }
-
-// interface IDispatchFromProps {
-//   addArticle: (article: IArticle) => void;
-// }
-
-// class Login extends Component<IClassProps> {
-//   public state = {
-//     email: '',
-//     password: '',
-//   };
-
-//   private loginFormRef = React.createRef<HTMLFormElement>() || null;
-
-//   public handleInputs = (event: React.FormEvent<HTMLInputElement>) => {
-//     this.setState({ [event.currentTarget.name]: event.currentTarget.value });
-//   };
-
-//   public onLoginIn = (event: React.FormEvent) => {
-//     const node = this.loginFormRef.current;
-//     if (node) {
-//       if (node.reportValidity()) {
-//         event.preventDefault();
-//         console.log(
-//           'Login Form is valid! ',
-//           this.state.email,
-//           this.state.password,
-//         );
-//         const id = uuid();
-//         const payload: IArticle = {
-//           id,
-//           name: 'blablabla',
-//         };
-//         this.props.addArticles(payload);
-//       }
-//     }
-//   };
-
-//   public render() {
-//     return (
-//       <>
-//         <Nav />
-//         <LoginPage
-//           email={this.state.email}
-//           password={this.state.password}
-//           loginFormRef={this.loginFormRef}
-//           onLoginIn={this.onLoginIn}
-//           handleInputs={this.handleInputs}
-//         />
-//       </>
-//     );
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch: Dispatch): IDispatchFromProps => ({
-//   addArticle: (article: any) => dispatch(addArticle(article)),
-// });
-
-// export default connect<null, IDispatchFromProps, void>(
-//   null,
-//   mapDispatchToProps,
-// )(Login);
+export default connect<null, IDispatchFromProps, void>(
+  null,
+  mapDispatchToProps,
+)(Login);
