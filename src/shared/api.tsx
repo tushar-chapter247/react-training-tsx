d031baa4459ffc37a0bdc6af473eacd01e190d71
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-29 16:23:20 +0530

    define type for devEnv value to solve issue

--- src/shared/api.tsx
+++ src/shared/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const devEnv = 'dev'; // dev for development and prod for production
+const devEnv: string = 'prod'; // dev for development and prod for production
 const SERVERURL: string =
-  devEnv === 'dev' ? 'https://reqres.in/api' : 'http://localhost:3000/prod';
+  devEnv === 'dev' ? 'https://reqres.in/api' : 'https://randomuser.me/api';
 
 export default axios.create({
   baseURL: SERVERURL,
