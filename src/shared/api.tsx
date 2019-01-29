d44b2ddd07de7349251aa7cf1e38b6855e237cc9
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-29 13:29:49 +0530

    Create axios base instance file

--- src/shared/api.tsx
+++ src/shared/api.tsx
@@ -1,3 +1,8 @@
+import axios from 'axios';
 const devEnv = 'dev'; // dev for development and prod for production
+const SERVERURL: string =
+  devEnv === 'dev' ? 'https://reqres.in/api' : 'http://localhost:3000/prod';
 
-export const SERVERURL: string = devEnv === 'dev' ? 'https://reqres.in/api' : 'http://localhost:3000/prod';
+export default axios.create({
+  baseURL: SERVERURL,
+});
