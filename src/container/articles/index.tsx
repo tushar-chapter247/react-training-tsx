e97fcf2dee8fe1433a67aaf3d99e9a8971ee5bf0
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-29 17:26:54 +0530

    Get value from store and show it in list

--- src/container/articles/index.tsx
+++ src/container/articles/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
+import { Dispatch, Reducer } from 'redux';
 import API from '../../shared/api';
 import { addArticle } from '../../store/actions';
 import Nav from '../nav';
@@ -11,11 +11,15 @@ interface IArticles {
   articles: any[];
 }
 
+interface IStateToProps {
+  storeArticles: any[]
+}
+
 interface IDispatchFromProps {
   articleActionDispatch: (payload: IArticles) => void;
 }
 
-interface IArticlePropTypes extends IDispatchFromProps {}
+interface IArticlePropTypes extends IStateToProps, IDispatchFromProps {}
 
 interface IStateTypes {
   articles: any[];
@@ -35,7 +39,7 @@ class Article extends Component<IArticlePropTypes, IStateTypes> {
       const response = await API.get('?results=20&format=json&inc=name,gender,location,email,phone,cell,dob,id,picture');
       const payload: IArticles = { articles: response.data.results }; // CREATE PAYLOAD TO SEND
       this.props.articleActionDispatch(payload); // STORE ALL ARTICLES IN STORE
-      this.setState({ articles: response.data.results });
+      // this.setState({ articles: response.data.results }); // WE WILL USE STORE TO SHOW ARTICLES
       return;
     } catch (error) {
       console.log('article error ', error);
@@ -46,17 +50,21 @@ class Article extends Component<IArticlePropTypes, IStateTypes> {
     return (
       <>
         <Nav />
-        <ArticlePage articles={this.state.articles} />
+        <ArticlePage articles={this.props.storeArticles} />
       </>
     );
   }
+}
+
+const mapStateToProps = (state: any): IStateToProps => {
+  return {storeArticles: state.articleReducer.articles}
 }
 
 const mapDispatchToProps = (dispatch: Dispatch): IDispatchFromProps => ({
   articleActionDispatch: (payload: IArticles) => dispatch(addArticle(payload)), // THIS FUNCTION WILL BE AVAILABLE IN PROPS
 });
 
-export default connect<null, IDispatchFromProps, void>(
-  null,
+export default connect<IStateToProps, IDispatchFromProps, void>(
+  mapStateToProps,
   mapDispatchToProps,
 )(Article);
