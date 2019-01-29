dd4132385c8ae3b6d158632771a8b489d8ba1028
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-29 13:30:03 +0530

    Use axios and async/await to call an api to fetch articles

--- src/container/articles/index.tsx
+++ src/container/articles/index.tsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import API from '../../shared/api';
 import ArticlePage from './article.page';
 
 class Article extends Component {
+  public componentDidMount = async () => {
+    await this.getArticles();
+  };
+
+  public getArticles = async () => {
+    try {
+      const response = await API.get('/users?page=2');
+      console.log('article response: ', response);
+      return;
+    } catch (error) {
+      console.log('article error ', error);
+    }
+  };
+
   public render() {
     return (
       <>
