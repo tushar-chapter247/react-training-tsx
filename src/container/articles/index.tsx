b9b2259f60f6c64fb2b716047ebc618e96e88c36
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-30 11:43:22 +0530

    Upload image file and show single as well as multiple selected image previews

--- src/container/articles/index.tsx
+++ src/container/articles/index.tsx
@@ -12,7 +12,7 @@ interface IArticles {
 }
 
 interface IStateToProps {
-  storeArticles: any[]
+  storeArticles: any[];
 }
 
 interface IDispatchFromProps {
@@ -23,11 +23,15 @@ interface IArticlePropTypes extends IStateToProps, IDispatchFromProps {}
 
 interface IStateTypes {
   articles: any[];
+  file: any;
+  multipleImages: any[];
 }
 
 class Article extends Component<IArticlePropTypes, IStateTypes> {
   public state = {
     articles: [],
+    file: '',
+    multipleImages: [],
   };
 
   public componentDidMount = async () => {
@@ -36,7 +40,9 @@ class Article extends Component<IArticlePropTypes, IStateTypes> {
 
   public getArticles = async () => {
     try {
-      const response = await API.get('?results=20&format=json&inc=name,gender,location,email,phone,cell,dob,id,picture');
+      const response = await API.get(
+        '?results=20&format=json&inc=name,gender,location,email,phone,cell,dob,id,picture',
+      );
       const payload: IArticles = { articles: response.data.results }; // CREATE PAYLOAD TO SEND
       this.props.articleActionDispatch(payload); // STORE ALL ARTICLES IN STORE
       // this.setState({ articles: response.data.results }); // WE WILL USE STORE TO SHOW ARTICLES
@@ -46,19 +52,44 @@ class Article extends Component<IArticlePropTypes, IStateTypes> {
     }
   };
 
+  public handleFileChange = (event: any) => {
+    const imgs: any[] = [...this.state.multipleImages];
+    imgs.push(URL.createObjectURL(event.target.files[0]));
+    console.log(event.target.files[0]);
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0]),
+      multipleImages: imgs,
+    });
+  };
+
   public render() {
     return (
       <>
         <Nav />
         <ArticlePage articles={this.props.storeArticles} />
+        <br />
+        <input type="file" onChange={this.handleFileChange} />
+        <h3>Selected image</h3>
+        <img src={this.state.file} />
+        <br />
+        <h3>Gallery</h3>
+        <div>
+          {this.state.multipleImages.length
+            ? this.state.multipleImages.map((k, i) => (
+              <>
+                <img src={k} />
+              </>
+            ))
+            : null}
+        </div>
       </>
     );
   }
 }
 
 const mapStateToProps = (state: any): IStateToProps => {
-  return {storeArticles: state.articleReducer.articles}
-}
+  return { storeArticles: state.articleReducer.articles };
+};
 
 const mapDispatchToProps = (dispatch: Dispatch): IDispatchFromProps => ({
   articleActionDispatch: (payload: IArticles) => dispatch(addArticle(payload)), // THIS FUNCTION WILL BE AVAILABLE IN PROPS
