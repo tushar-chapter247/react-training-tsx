6bdc86272ac9a59e561018cb5b8a57c316d014c2
Ayushi Jain <ayushij.chapter247@gmail.com>
2019-01-30 19:52:07 +0530

    Create action for signup succes and signup failure by calling api

--- src/store/actions/account/index.tsx
+++ src/store/actions/account/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { AnyAction } from 'redux';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import * as actions from '../action-types';
@@ -35,8 +36,32 @@ export const userSignupReq = (
     return new Promise<void>(resolve => {
       console.log('action user signup req: ', payload);
       // call signup api here and dispatch actions according to response
+      axios.post('https://reqres.in/api/login', {
+        "email": "peter@klaven",
+        "password": "cityslicka"
+      })
+      .then((response) => {
+        dispatch(signupReqSuccess(response))
+      })
+      .catch((err) => {
+        dispatch(signupReqError(err))
+      })
       resolve();
     });
+  };
+};
+
+export const signupReqSuccess = (response: any) => {
+  return {
+    signupResponse: response, // AS MENTIONED IN REDUCERS
+    type: actions.USER_SIGNUP_SUCCESS,
+  };
+};
+
+export const signupReqError = (error: any) => {
+  return {
+    signupError: error, // AS MENTIONED IN REDUCERS
+    type: actions.USER_SIGNUP_FAILURE,
   };
 };
 
