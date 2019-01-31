81a09b9a480a59be842c10a10859fcbc524929e2
Ayushi Jain <ayushij.chapter247@gmail.com>
2019-01-31 19:36:32 +0530

    Show product details page with data

--- src/container/product-details/index.tsx
+++ src/container/product-details/index.tsx
@@ -1,13 +1,39 @@
+import axios from 'axios';
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import Nav from '../nav';
 import ProductDetailsPage from './product-details.page';
 
-class ProductDetails extends Component {
+interface IProps extends RouteComponentProps<any> {};
+interface IStates {
+  userData: any;
+};
+
+class ProductDetails extends Component<IProps, IStates> {
+  public state = {
+    userData: null
+  }
+
+  public componentDidMount = async() => {
+    await this.getRandomUser();
+  }
+
+  public getRandomUser = async() => {
+    await axios.get('https://randomuser.me/api/0.4/?randomapi')
+      .then((res: any) => {
+        console.log('user data: ', res.data.results[0].user);
+        this.setState({
+          userData: res.data.results[0].user
+        });
+      })
+      .catch(err => console.log('user data error: ', err));
+  }
+
   public render () {
     return (
       <>
         <Nav />
-        <ProductDetailsPage />
+        <ProductDetailsPage userData={this.state.userData} />
       </>
     );
   }
--- src/container/product-details/product-details.page.tsx
+++ src/container/product-details/product-details.page.tsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ShareIcon from '@material-ui/icons/Share';
+import React from 'react';
+import './product-details.css';
 
-const ProductDetailsPage = () => {
+interface IPropTypes {
+  userData: any
+}
+
+const ProductDetailsPage = (props: IPropTypes) => {
   return (
     <>
-      <h2>This is product details page</h2>
+      <h2>
+        This is product details page, showing any random data as due to fake api
+      </h2>
+      <br />
+      <Card className="pcard">
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className="pavatar">
+              {props.userData && props.userData.name ? props.userData.name.first.charAt(0).toUpperCase() : null}
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={props.userData && props.userData.name ? props.userData.name.first + ' ' + props.userData.name.last : null}
+          subheader={props.userData && props.userData.email ? props.userData.email : null}
+        />
+        <CardMedia
+          className="pmedia"
+          image={props.userData && props.userData.picture ? props.userData.picture : "/static/images/cards/paella.jpg"}
+          title="Paella dish"
+        />
+        <CardContent>
+          <Typography component="p">
+            {props.userData && props.userData.location ? props.userData.location.street + ' ' + props.userData.location.city + ' ' + props.userData.location.state + ' ' + props.userData.location.zip : <strong>Loading Data ...</strong>}
+          </Typography>
+        </CardContent>
+        <CardActions className="pactions" disableActionSpacing={true}>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
     </>
   );
-}
+};
 
 export default ProductDetailsPage;
